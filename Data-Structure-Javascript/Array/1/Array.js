fb0bcfc88002ddf04d7ea7c1f9908d5564f29e16
Manish Jain <Manish.Jain@hcentive.com>
2016-04-06 19:52:49 +0530

    +commit `date -R`

--- Data-Structure-Javascript/Array/1/Array.js
+++ Data-Structure-Javascript/Array/1/Array.js
@@ -22,5 +22,6 @@ console.log('names1         ',names1);
 console.log('names1.indexOf("Cynthia")       ',names1.indexOf('Cynthia'))
 var names2 = ["David", "Cynthia", "Raymond", "Clayton", "Jennifer"];
 console.log('names2.join()                ',names2.join());
+
 console.log('names2.toString()                ',names2.toString());
 
