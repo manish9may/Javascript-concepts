24967afb9e8570c73e8b00969047b895034f2ab5
Manish Jain <manish9may@gmail.com>
2016-04-21 03:36:03 +0530

    +commit Thu, Apr 21, 2016  3:36:02 AM

--- Data-Structure-Javascript/List/1/List.js
+++ Data-Structure-Javascript/List/1/List.js
@@ -1,84 +1,47 @@
 var List = function(){
-	this.dataStore = [];
-	this.listSize = 0;
-	this.pos = 0;
+	this.$dataStore = [];
+	this.$size = 0;
 }
-
-List.prototype.append = function(element){
-	this.dataStore[this.listSize++] = element;
-}
-List.prototype.find = function(element){
-	for(var itemIndex = 0,len = this.listSize;itemIndex < len ; ++itemIndex){
-		if(this.dataStore[itemIndex] === element){
-			return itemIndex;
+List.prototype = {
+	append : function($element){
+		this.$dataStore[this.$size++] = $element;
+	},
+	find : function($element){
+		/*Array.prototype.forEach.call(this.dataStore,function(element,index){
+			if(element === $element){
+				return index;
+			}
+		});*/
+		return Array.prototype.findIndex.call(this.$dataStore,function(element,index){return element === $element});
+	},
+	remove : function($element){
+		var $itemIndex = this.find($element);
+		if($itemIndex > -1){
+			this.size -- ;
+			return Array.prototype.splice.call(this.$dataStore,$itemIndex,1);
+		}
+		throw new Error('Not Found!');
+	},
+	length : function(){
+		return this.$size;
+	},
+	toString : function(){
+		return this.$dataStore.toString();
+	},
+	clear : function(){
+		Array.prototype.splice.call(this.$dataStore,0,this.$size);
+		this.size = 0;
+	},
+	insertAfter : function($element,$afterElement){
+		var $itemIndex = this.find($afterElement);
+		if($itemIndex > -1){
+			Array.prototype.splice.call(this.$dataStore,$itemIndex + 1,0,$element);
+			return this.$size++;
+		}else{
+			throw new Error('Not Found!');		
 		}
 	}
-	return -1;
-}
-List.prototype.remove = function(element){
-	var itemIndex = this.find(element);
-	if(itemIndex > -1){
-		this.dataStore.splice(itemIndex,1);
-		this.listSize --;
-		return true;
-	}
-	return false;
-}
-List.prototype.length = function(){
-	return this.listSize;
-}
-List.prototype.toString = function(){
-	return this.dataStore.toString();
-}
-
-List.prototype.clear = function(){
-	delete this.dataStore;
-	this.dataStore = [];
-	this.listSize = 0;
-	this.pos = 0;
-}
-List.prototype.insertAfter = function(element,afterElement){
-	var itemIndex = this.find(afterElement);
-	if(itemIndex > -1){
-		this.dataStore.splice(itemIndex + 1,0,element);
-		this.listSize ++;
-		return true;
-	}
-	return false;
-}
-
-List.prototype.contains = function(element){
-	var itemIndex = find(element);
-	if(itemIndex > -1){
-		return true;
-	}
-	return false;
-}
-List.prototype.front = function(){
-	this.pos = 0;
-}
-List.prototype.end = function(){
-	this.pos = this.listSize - 1;
-}
-List.prototype.prev = function(){
-	if(this.pos > 0){
-		--this.pos;
-	}
-}
-List.prototype.next = function(){
-	if(this.pos <= this.listSize - 1){
-		++ this.pos;
-	}
-}
-List.prototype.currentPos = function(){
-	return this.pos;
-}
-List.prototype.moveToPos = function(position){
-	this.pos = position;
-}
-List.prototype.getElement = function(){
-	return this.dataStore[this.pos];
-}
+};
 
 
 var names = new List();
@@ -88,17 +51,12 @@ names.append("Cynthia");
 names.append("Jennifer");
 names.append("Bryan");
 names.append("Danny");
+console.log('List.toString()			',names.toString());
+console.log('List.find("Raymond")			',names.find("Raymond"));
+console.log('List.insertAfter("Manish Jain","Raymond")			',names.insertAfter("Manish Jain","Raymond"));
 
-names.front();
-console.log(names.getElement());
-names.next();
-console.log(names.getElement());
+console.log('List.toString()			',names.toString(),'\n');
 
-names.next();
-names.next();
-names.prev();
-console.log(names.getElement());
-console.log("####################################")
-for(names.front();names.currentPos() < names.length();names.next()){
-	console.log(names.getElement())
-}
+console.log('List.remove()			',names.remove("Bryan"));
+
+console.log('List.toString()			',names.toString(),'\n');
