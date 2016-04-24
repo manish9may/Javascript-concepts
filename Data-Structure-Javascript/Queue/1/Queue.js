6ed4d201848a46881d2a5f90913bea38c9a7741b
Manish Jain <manish9may@gmail.com>
2016-04-24 17:18:58 +0530

    +commit queue Sun, Apr 24, 2016  5:18:58 PM

--- Data-Structure-Javascript/Queue/1/Queue.js
+++ Data-Structure-Javascript/Queue/1/Queue.js
@@ -1,90 +1,98 @@
 var Queue = function(){
-	this.dataStore = [];
+	this.$dataStore = [];
 }
-Queue.prototype.enqueue = function(element){
-	this.dataStore.push(element);
-}
-Queue.prototype.dequeue = function(){
-	return this.dataStore.shift();
-}
-Queue.prototype.front = function(){
-	return this.dataStore[0];
-}
-Queue.prototype.back = function(){
-	return this.dataStore[this.dataStore.length - 1];
-}
-Queue.prototype.empty = function(){
-	return this.dataStore.length == 0 ? true : false;
-}
-Queue.prototype.toString = function(){
-	return this.dataStore.toString();
-}
-Queue.prototype.dequeueOnPriority = function(key){
-	var priority = this.dataStore[0][key];
-	for(var index = 1; index < this.dataStore.length ; ++index){
-		if(this.dataStore[index][key] < priority){
-			priority = index; 
-		}
+Queue.prototype = {
+	enqueue : function($element){
+		this.$dataStore.push($element);
+	},
+	dequeue : function(){
+		return this.$dataStore.shift();
+	},
+	front : function(){
+		return this.$dataStore[0];
+	},
+	back : function(){
+		return this.$dataStore[this.$dataStore.length - 1];
+	},
+	toString : function(){
+		return this.$dataStore.toString();
+	},
+	empty : function(){
+		return this.$dataStore.length === 0;
+	},
+	dequeueOnPriority : function(key){
+		var priority = this.$dataStore[0][key];
+		Array.prototype.forEach.call(this.$dataStore,function(data,index){
+			if(data[key] < priority){
+				priority = data[key];
+			}
+		});
+		var priorityIndex = Array.prototype.findIndex.call(this.$dataStore,function(data,index){
+						return data[key] === priority;
+		});
+		return Array.prototype.splice.call(this.$dataStore,priorityIndex,1);
 	}
-	return this.dataStore.splice(priority,1);
 }
+
 var q = new Queue();
 q.enqueue("Meredith");
 q.enqueue("Cynthia");
 q.enqueue("Jennifer");
-console.log(q.toString());
+console.log('q.toString()			',q.toString(),'\n');
 q.dequeue();
-console.log(q.toString());
+console.log('q.toString()			',q.toString(),'\n');
 
 console.log("Front of queue: " + q.front());
 console.log("Back of queue: " + q.back());
-
-
-var distribute = function(nums,queues,n,digit){
-	for(var i = 0;i < n; ++i){
-		if(digit == 1){
-			queues[nums[i]%10].enqueue(nums[i])
-		}else{
-			queues[Math.floor(nums[i]/10)].enqueue(nums[i]);
-		}
-	}
-}
-
-var collect = function(queues,nums,n){
-	var k = 0;
-	for(var i = 0;i < n; ++i){
-		while(!queues[i].empty()){
-			nums[k++] =  queues[i].dequeue();
-		}
-	}
-}
-function dispArray(arr) {
-	console.log(arr.toString());
-}
+console.log('\n######### Radix Sort ##########\n')
+var nums = [];
 var queues = [];
 for (var i = 0; i < 10; ++i) {
+nums[i] = Math.floor(Math.floor(Math.random() * 101));
 queues[i] = new Queue();
 }
-var nums = [];
-for (var i = 0; i < 10; ++i) {
-nums[i] = Math.floor(Math.floor(Math.random() * 101));
-}
+
 console.log("\n\nBefore radix sort: ");
-dispArray(nums);
-distribute(nums, queues, 10, 1);
-collect(queues, nums,10);
-distribute(nums, queues, 10, 10);
-collect(queues, nums,10);
+console.log(nums.toString())
+
+var distribte = function(numbers,queues,digit){
+	Array.prototype.forEach.call(numbers,function(object,index){
+		if(digit === 1){
+				queues[numbers[index] % 10].enqueue(object);
+		}else{
+			queues[Math.floor(numbers[index] /= 10)].enqueue(object);
+		}		
+	});
+}
+
+var collect = function(queues,numbers){
+	var k = 0;
+	Array.prototype.splice.call(numbers,0,numbers.length);
+	Array.prototype.forEach.call(queues,function(queue,index){
+		while(!queue.empty()){
+			Array.prototype.splice.call(numbers,k,0,queue.dequeue());
+			k++;
+		}
+	});
+}
+
+distribte(nums,queues,1);
+collect(queues,nums)
+distribte(nums,queues,10);
+collect(queues,nums)
 console.log("\n\nAfter radix sort: ");
-dispArray(nums);
+console.log(nums.toString());
 
-
+console.log('\n########### Priority Queue ###########\n')
 var Patient = function(name,code){
 	this.name = name;
 	this.code = code;
 }
-Patient.prototype.toString = function(){
-	return this.name+' code: '+this.code;
+
+Patient.prototype = {
+	toString : function(){
+		return this.name+' code: '+this.code;
+	}
 }
 
 var p = new Patient("Smith",5);
@@ -99,15 +107,19 @@ ed.enqueue(p);
 p = new Patient("Ingram", 1);
 ed.enqueue(p);
 console.log('\n\n'+ed.toString());
+
 var seen = ed.dequeueOnPriority('code');
 console.log('\n\n'+"Patient being treated: " + seen[0].name);
 console.log("Patients waiting to be seen: ")
 console.log(ed.toString());
+
 // another round
 var seen = ed.dequeueOnPriority('code');
 console.log('\n\n'+"Patient being treated: " + seen[0].name);
 console.log("Patients waiting to be seen: ")
 console.log(ed.toString());
+
+// another round
 var seen = ed.dequeueOnPriority('code');
 console.log('\n\n'+"Patient being treated: " + seen[0].name);
 console.log("Patients waiting to be seen: ")
