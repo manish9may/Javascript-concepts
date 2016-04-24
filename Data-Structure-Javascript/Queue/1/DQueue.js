12cca393b097ee474d9e120209657560f721079e
Manish Jain <manish9may@gmail.com>
2016-04-24 19:12:58 +0530

    +commit dqueue Sun, Apr 24, 2016  7:12:57 PM

--- Data-Structure-Javascript/Queue/1/DQueue.js
+++ Data-Structure-Javascript/Queue/1/DQueue.js
@@ -1,38 +1,41 @@
 var DQueue = function(){
-	this.dataStore = [];
+	this.$dataStore = [];
 }
-DQueue.prototype.enqueue = function(element){
-	this.dataStore.push(element);
-}
-DQueue.prototype.dequeue = function(){
-	return this.dataStore.shift();
-}
-DQueue.prototype.front = function(){
-	return this.dataStore[0];
-}
-DQueue.prototype.back = function(){
-	return this.dataStore[this.dataStore.length - 1];
-}
-DQueue.prototype.empty = function(){
-	return this.dataStore.length == 0 ? true : false;
-}
-DQueue.prototype.toString = function(){
-	return this.dataStore.toString();
-}
-DQueue.prototype.insertAtFront = function(element){
-	this.dataStore.unshift(element);
-}
-DQueue.prototype.deleteAtLast = function(){
-	return this.dataStore.pop();
+
+DQueue.prototype = {
+	enqueue : function($element){
+		this.$dataStore.push($element);
+	},
+	dequeue : function(){
+		return this.$dataStore.shift();
+	},
+	front : function(){
+		return this.$dataStore[0];
+	},
+	back : function(){
+		return this.$dataStore[this.$dataStore.length - 1];
+	},
+	empty : function(){
+		return this.$dataStore.length === 0;
+	},
+	toString : function(){
+		return this.$dataStore.toString();
+	},
+	insertAtFront : function($element){
+		this.$dataStore.unshift($element);
+	},
+	deleteFromLast : function(){
+		this.$dataStore.pop();
+	}
 }
 
 var q = new DQueue();
 q.enqueue("Meredith");
 q.enqueue("Cynthia");
 q.enqueue("Jennifer");
-console.log(q.toString());
-q.dequeue();
-console.log(q.toString());
+console.log('q.toString()				',q.toString(),'\n');
+console.log('q.dequeue()				',q.dequeue(),'\n');
+console.log('q.toString()				',q.toString(),'\n');
 q.insertAtFront('Jasica');
 
 console.log("Front of queue: " + q.front());
