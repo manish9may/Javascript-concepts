a9b8ddbb5593d1d3a69d09640411c347c1e160f6
Manish Jain <manish9may@gmail.com>
2016-08-01 11:19:01 +0530

    Mon, Aug  1, 2016 11:19:00 AM

--- javscript-application/web-app/production-env/min/messages/messages.js
+++ javscript-application/web-app/production-env/min/messages/messages.js
@@ -5,5 +5,6 @@ var messages = {
 	'label.layout.application.nav.algo.title' : 'Javascript Algorithms',
 	'label.layout.application.nav.layout.sub.title1' : 'Layout 001',
 	'label.layout.application.nav.layout.sub.title2' : 'Layout 002',
-	'label.layout.application.footer.title' : 'Copyright &copy; @manish. All rights reserved'
+	'label.layout.application.footer.title' : 'Copyright &copy; @manish. All rights reserved',
+	'label.layout.application.nav.home.page.header' : 'Home'
 }
